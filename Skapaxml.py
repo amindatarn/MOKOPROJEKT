6a6ad9c6e7fdf2e197878e003b67a92893f9add4
amindatarn <68318132+amindatarn@users.noreply.github.com>
2020-07-27 07:59:46 +0200

    Add files via upload

--- Skapaxml.py
+++ Skapaxml.py
@@ -6,7 +6,7 @@ sheet = wb.sheet_by_index(0)
 
 def createxmlmall(i):
     # build a tree structure
-    root = "root" + str(i)
+    root = "root"
     root = ET.Element("state")
 
     model = ET.SubElement(root, "model")
@@ -20,17 +20,11 @@ def createxmlmall(i):
     safecookie = ET.SubElement(root, "safecookie")
 
     safestep1 = ET.SubElement(safecookie, "safe-step", name= "Project Information")
-    safestep2 = ET.SubElement(safecookie, "safe-step", name="Indata")
 
     commits1 = ET.SubElement(safestep1, "commits")
-    commits2 = ET.SubElement(safestep2, "commits")
 
     steps = ET.SubElement(root, "steps")
     prev = ET.SubElement(steps, "prev")
-    step = ET.SubElement(prev, "step").text =("Project Information")
-
-    current = ET.SubElement(steps, "current").text=("Indata")
-    next = ET.SubElement(steps, "next")
 
     lastproxy = ET.SubElement(root, "last-proxy").text="tcserver0"
 
@@ -38,27 +32,30 @@ def createxmlmall(i):
 
     tree = ET.ElementTree(root)  # sparar hela trädet i variabeln tree
 
-    return tree, commits1, commits2,filnamn
+    return tree, commits1, filnamn,safecookie,steps,prev
 
-def getdatafromexcel(commits1, projectnumber, moduletype, columnsformat, stiffener, X1, Y1, Z1, Y2, Y3,): #lägger in varibler
+def createxmlmalldefault(steps,prev):
+    current = ET.SubElement(steps, "current").text = ("Project Information")
+    next = ET.SubElement(steps, "next")
+    step = ET.SubElement(next, "step").text = ("Indata")
+
+def createxmlmallejdefault(i,safecookie,steps,prev):
+    safestep2 = ET.SubElement(safecookie, "safe-step", name="Indata")
+    commits2 = ET.SubElement(safestep2, "commits")
+
+    step = ET.SubElement(prev, "step").text = ("Project Information")
+    current = ET.SubElement(steps, "current").text = ("Indata")
+    next = ET.SubElement(steps, "next")
+    return commits2
+
+def getdatafromexcel(commits1, projectnumber, moduletype, columnsformat, stiffener, X1,Y1,Z1): #lägger in varibler
 
     indatatillxml(commits1, "Project_number", "007")
-    indatatillxml(commits1, "Module_Type", str(moduletype))
-    indatatillxml(commits1, "Module_number",str(projectnumber))
     indatatillxml(commits1, "Ritad_Av", "AMINDA TÄRN")
     indatatillxml(commits1, "Uppdragsansvarig","PATRIK JENSEN")
     indatatillxml(commits1, "Status", "FOR PRODUCTION")
     indatatillxml(commits1, "Datum", "2020-09-11")
     indatatillxml(commits1, "Regulations", "001001")
-    indatatillxml(commits1, "Columns", columnsformat)
-    indatatillxml(commits1, "Stiffener", stiffener)
-
-    indatatillxml(commits1, "X1", str(X1))
-    indatatillxml(commits1, "Y1", str(Y1))
-    indatatillxml(commits1, "Y2", str(Y2))
-    indatatillxml(commits1, "Y3", str(Y3))
-    indatatillxml(commits1, "Z1", str(Z1))
-
 
 def indatatillxml(commit ,variabel1,variabel2): #skapar alla attribut för commits1
     ET.SubElement(commit, "committed", name=variabel1).text = variabel2
@@ -86,40 +83,48 @@ def makeinputstring(variabel, i, f):
     else:
         return str(variabel)
 
+    def formatStiffener(stiffener):
+        if stiffener == "Right":
+            return "RS"
+        elif stiffener == "Left":
+            return "LS"
+        else:
+            return "NS"
+
 
 def commit1(i, commits1): #läser raderna i vald excelfil
     modulenumber = sheet.cell_value(i, 0)
-    projectnumber = modulenumber[2:] #läser av modulnumer
-    moduletype = modulenumber[0:2] #läser av modultypen
 
-    columns = makeinputstring("columns",i,1)
+    indatatillxml("commits1", "Module_number", modulenumber[2:])  # läser av modulnumer
+    indatatillxml("commits1", "Mudule_Type", modulenumber[0:2])  # läser av modultypen
+    indatatillxml("commits1", "Columns", makeinputstring("columns",i,1))
+
     columnsformat = columns + "P"
 
-    stiffener = sheet.cell_value(i,2) #läser av stiffener och gör om det till rätt format.
-    if stiffener == "Right":
-        stiffener = "RS"
-    elif stiffener == "Left":
-        stiffener = "LS"
-    else:
-        stiffener = "NS"
+    indatatillxml("commits1", "Stiffener", formatStiffener(excelPathCellValue(i, 2)))
 
-    X1 = makeinputstring("X1",i,3)
-    Y1 = makeinputstring("Y1",i,4)
-    Z1 = makeinputstring("Z1",i,7)
+"""
+stiffener = sheet.cell_value(i,2) #läser av stiffener och gör om det till rätt format.
+if stiffener == "Right":
+    stiffener = "RS"
+elif stiffener == "Left":
+    stiffener = "LS"
+else:
+    stiffener = "NS" 
+"""
 
-    if columns == "6":  #lägger in värdet för olika antal dörrar
-        Y2 = makeinputstring("Y2",i,5)
-        Y3 = ""
+    indatatillxml("commits1", "X1", makeinputstring("X1", i, 3))
+    indatatillxml("commits1", "Y1", makeinputstring("Y1", i, 4))
+    indatatillxml("commits1", "Z1", makeinputstring("Z1", i, 7))
+
+    if columns == "6":  # lägger in värdet för olika antal dörrar
+        indatatillxml("commits1", "Y2", makeinputstring("Y2", i, 5))
     elif columns == "8":
-        Y2 = makeinputstring("Y2",i,5)
-        Y3 = makeinputstring("Y3",i,6)
-    else:
-        Y2 = ""
-        Y3 = ""
+        indatatillxml("commits1","Y2", makeinputstring("Y2", i, 5))
+        indatatillxml("commits1","Y3", makeinputstring("Y3", i, 6))
 
     filnamn = "007" + modulenumber.zfill(3) + "_"
-    getdatafromexcel(commits1, projectnumber, moduletype, columnsformat, stiffener, X1, Y1, Z1, Y2, Y3)
-    print(projectnumber, moduletype, columnsformat, stiffener, X1, Y1, Y2, Y3, Z1)
+
     return filnamn
 
 
@@ -165,12 +170,12 @@ class Commit2:
         self.name = name
         self.text = text
 
-    def addtoxml(self,commits2):
-        indatatillxml(commits2,self.name ,self.text)
+    def addtoxml(self,commit):
+        indatatillxml(commit,self.name ,self.text)
 
-def commit2(i,commits2,x):
+def commit2(i,x, commits2):
 
-    wall = Commit2("Wall_number", str(x))
+    wall = Commit2("Wall_Number", str(x))
     wall.addtoxml(commits2)
 
     ventholecheck(i,commits2)
@@ -181,58 +186,74 @@ def commit2(i,commits2,x):
     s = (sheet.cell_value(i, 9)).lower()  # sänker all wallsetup till små bokstäver
 
     if s == "straight":
-        wallsetup = Commit2("Wall_setup", "S")
+        wallsetup = Commit2("Wall_Setup", "S")
         wallsetup.addtoxml(commits2)
 
     elif s == "door":
-        wallsetup = Commit2("Wall_setup", "D")
+        wallsetup = Commit2("Wall_Setup", "D")
         wallsetup.addtoxml(commits2)
-        makeadoor(i, 15, 16, 17,1,commits2)
+        makeadoor(i, 15, 16, 17,1,commits2,"Door_Type","X_Door","Door1 Serup")
 
     elif s == "window":
-        wallsetup = Commit2("Wall_setup", "W")
+        wallsetup = Commit2("Wall_Setup", "W")
         wallsetup.addtoxml(commits2)
-        makewindow(i,21,22,23,1,commits2)
+        makewindow(i,21,22,23,1,commits2,"Window_Type", "X_Window", "Window_Sill")
 
     elif s == "window and door":
-        wallsetup = Commit2("Wall_setup", "DRWL")
+        wallsetup = Commit2("Wall_Setup", "DRWL")
         wallsetup.addtoxml(commits2)
-        makewindow(i, 21, 22, 23, 1, commits2)
-        makeadoor(i, 15, 16, 17, 1, commits2)
+        makewindow(i, 21, 22, 23, 1, commits2,"Window_Type", "X_Window", "Window_Sill")
+        makeadoor(i, 15, 16, 17, 1, commits2,"Door_Type","X_Door","Door1 Serup")
 
     elif s == "door and window":
-        wallsetup = Commit2("Wall_setup", "DLWR")
+        wallsetup = Commit2("Wall_Setup", "DLWR")
         wallsetup.addtoxml(commits2)
-        makewindow(i, 21, 22, 23, 1, commits2)
-        makeadoor(i, 15, 16, 17, 1, commits2)
+        makewindow(i, 21, 22, 23, 1, commits2,"Window_Type", "X_Window", "Window_Sill")
+        makeadoor(i, 15, 16, 17, 1, commits2,"Door_Type","X_Door","Door1 Serup")
 
     elif s == "door and door":
-        wallsetup = Commit2("Wall_setup", "DD")
+        wallsetup = Commit2("Wall_Setup", "DD")
         wallsetup.addtoxml(commits2)
-        makeadoor(i, 15, 16, 17, 1, commits2)
-        makeadoor(i, 18, 19, 20, 2, commits2)
+        makeadoor(i, 15, 16, 17, 1, commits2,"Door_Type","X_Door","Door1 Serup")
+        makeadoor(i, 18, 19, 20, 2, commits2,"Door2_Type","X_Door2","Door2 Setup")
 
     elif s == "window and window":
-        wallsetup = Commit2("Wall_setup", "WW")
+        wallsetup = Commit2("Wall_Setup", "WW")
         wallsetup.addtoxml(commits2)
-        makewindow(i, 21, 22, 23, 1, commits2)
-        makewindow(i, 24, 25, 26, 2, commits2)
+        makewindow(i, 21, 22, 23, 1, commits2,"Window_Type", "X_Window", "Window_Sill")
+        makewindow(i, 24, 25, 26, 2, commits2,"Window2_Type", "X_Window_2","Window_Sill_2")
 
     else:
         wallsetup = "FELLLLLL"
 
-def makeadoor(i,a,b,c,n,commits2):
-    doorsize = Commit2("Door" + str(n) + "_Type", ("D_" + (sheet.cell_value(i, a))))
-    placementdoor = Commit2("X_Door" + str(n), str(int(sheet.cell_value(i, b))))
-    dsetup = Commit2("Door" + str(n) + "setup", str(doorsetup(i, c)))
+def rounding(x):
+    if len(x) == 3:
+        return int(round(int(x) / 100.0)) * 100
+    elif len(x) == 4:
+        return int(round(int(x) / 100.0)) * 100
+
+def windowanddoorsize(a):
+    list = []
+
+    for b in a.split("x"):
+        b = rounding(b)
+        list.append(str(b))
+    return "x".join(list)
+
+def makeadoor(i,a,b,c,n,commits2, doortype, xdoor, doorserup):
+    avrundadstrl = windowanddoorsize(sheet.cell_value(i, a))
+    doorsize = Commit2(doortype, ("D_" + avrundadstrl))
+    placementdoor = Commit2(xdoor, str(int(sheet.cell_value(i, b))))
+    dsetup = Commit2(doorserup, str(doorsetup(i, c)))
     doorsize.addtoxml(commits2)
     placementdoor.addtoxml(commits2)
     dsetup.addtoxml(commits2)
 
-def makewindow(i,a,b,c,n,commits2):
-    windowsize = Commit2("Window" + str(n) + "_Type", "W_" + (sheet.cell_value(i, a)))
-    placementwindow = Commit2( "X_Window", str(int(sheet.cell_value(i, b))))
-    windowsill = Commit2("Window_sill",str(sheet.cell_value(i, c)))
+def makewindow(i,a,b,c,n,commits2,windowtype,xwindow,windowsill):
+    avrundadstrl = windowanddoorsize(sheet.cell_value(i, a))
+    windowsize = Commit2(windowtype, "W_" + avrundadstrl)
+    placementwindow = Commit2(xwindow, str(int(sheet.cell_value(i, b))))
+    windowsill = Commit2(windowsill,str(sheet.cell_value(i, c)))
     windowsize.addtoxml(commits2)
     placementwindow.addtoxml(commits2)
     windowsill.addtoxml(commits2)
@@ -243,19 +264,22 @@ def gothroughexcel():
     while i < (sheet.nrows):
         if len(sheet.cell_value(i, 0)) == 5 and sheet.cell_value(i, 0)[0:2] == "VA": #and sheet.cell_value(i,9) != "":  # sorterar ut väggar som som har commits1data
             columns = makeinputstring("columns", i, 1)  # kontroll av columns
-            tree, commits1, commits2, filnamn = createxmlmall(i)
-            savexml(tree, filnamn + "default" + ".xml")
+            tree, commits1, filnamn,safecookie,steps,prev = createxmlmall(i)
+            createxmlmalldefault(steps, prev)
+            savexml(tree, filnamn + "default" + ".tcs")
             x=1
             k=i
             while x<= int(columns):
                 if not sheet.cell_value(k, 15)[0:2].lower() == "sa": #sorterar bort alla väggar som är "samma som"
-                    tree, commits1, commits2, filnamn = createxmlmall(i)
-                    commit2(k, commits2, x)
+                    tree, commits1, filnamn,safecookie,steps,prev = createxmlmall(i)
+                    commits2 = createxmlmallejdefault(i, safecookie, steps, prev)
+                    commit2(k, x, commits2)
                     savexml(tree, filnamn + "20300" + str(x).zfill(3) + ".xml")
                 k+=1
                 x+=1
         else:
-            print(i)
+            pass
+            #print(i)
         i+=1
 
 
